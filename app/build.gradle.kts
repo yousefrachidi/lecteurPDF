9958b84c6eb75faca1ff37f90f01299638531bb6
Mohcine Elfahli <yourath3@gmail.com>
2026-06-06 12:27:29 +0100

    add file .yaml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.lecteurpdf"
+        applicationId = "youra.lecteur.pdf"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
